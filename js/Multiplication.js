85110c09d89de7697918a519d6c2b497940d6e16
vetlewoie <vet.h.woie@hotmail.com>
2019-01-19 01:34:04 +0100

    ll js files in own folder

---
+++ js/Multiplication.js
@@ -0,0 +1,85 @@
+function newMatrix(inputMatrix, rows, columns){
+    var i,j;
+    var matrix = new Matrix(rows, columns);
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < columns; j++){
+            matrix.mat[i][j] = parseInt(inputMatrix[i][j].value);
+        }
+    }
+    return matrix;
+}
+
+function removeChildNodes(container){
+    while(container.hasChildNodes()){
+        container.removeChild(container.lastChild);
+    }
+}
+
+function multiplication(){
+    var rowsA = parseInt(document.getElementById('rowsA').value);
+    var columnsA =parseInt(document.getElementById('columnsA').value);
+    var rowsB = parseInt(document.getElementById('rowsB').value);
+    var columnsB =parseInt(document.getElementById('columnsB').value);
+
+    if(columnsA != rowsB){
+        alert("Columns of matrix A must equal rows of matrix B");
+        location.reload();
+    }
+
+    var inputA = [];
+    var inputB = [];
+
+    var i;
+    var j;
+    var h2;
+    var divA;
+    var divB;
+
+    removeChildNodes(input1);
+    removeChildNodes(input2);
+    removeChildNodes(answer);
+
+    h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode("Matrix A:"));
+    input1.appendChild(h2);
+
+    h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode("Matrix B:"));
+    input2.appendChild(h2);
+
+    for(i = 0; i < rowsA; i++){
+        inputA[i] = [];
+        divA = document.createElement("div");
+        input1.appendChild(divA);
+        for(j = 0; j < columnsA; j++){
+            inputA[i].push(document.createElement("input"));
+            inputA[i][j].type="text";
+            divA.appendChild(inputA[i][j]);
+        }
+    }
+
+    for(i = 0; i < rowsB; i++){
+        inputB[i] = [];
+        divB = document.createElement("div");
+        input2.appendChild(divB);
+        for(j = 0; j < columnsB; j++){
+            inputB[i].push(document.createElement("input"));
+            inputB[i][j].type="text";
+            divB.appendChild(inputB[i][j]);
+        }
+    }
+
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Submit"));
+    button.id = "second";
+    answer.appendChild(button);
+    button.onclick = function(){
+        removeChildNodes(answer);
+        var matA = newMatrix(inputA,rowsA,columnsA);
+        var matB = newMatrix(inputB,rowsB,columnsB);
+        var matC = matA.mul(matB);
+        matC.tohtml(answer);
+    
+        console.log(matC.tostring());
+    }
+}
---
+++ js/trace.js
@@ -0,0 +1,60 @@
+function newMatrix(inputMatrix, rows, columns){
+    var i,j;
+    var matrix = new Matrix(rows, columns);
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < columns; j++){
+            matrix.mat[i][j] = parseInt(inputMatrix[i][j].value);
+        }
+    }
+    return matrix;
+}
+
+function removeChildNodes(container){
+    while(container.hasChildNodes()){
+        container.removeChild(container.lastChild);
+    }
+}
+
+
+function trace(){
+    var rows = parseInt(document.getElementById('rows').value);
+    var columns =parseInt(document.getElementById('columns').value);
+
+    var input1 = document.getElementById("input1");
+    var answer = document.getElementById("answer");
+
+    var inputA = [];
+
+    var i;
+    var j;
+    var h2;
+    var divA;
+
+    removeChildNodes(input1);
+    removeChildNodes(answer);
+
+    h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode("Matrix:"));
+    input1.appendChild(h2);
+
+    for(i = 0; i < rows; i++){
+        inputA[i] = [];
+        divA = document.createElement("div");
+        input1.appendChild(divA);
+        for(j = 0; j < columns; j++){
+            inputA[i].push(document.createElement("input"));
+            inputA[i][j].type="text";
+            divA.appendChild(inputA[i][j]);
+        }
+    }
+
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Submit"));
+    button.id = "second";
+    answer.appendChild(button);
+    button.onclick = function(){
+        removeChildNodes(answer);
+        var matA = newMatrix(inputA,rows,columns);
+        answer.appendChild(document.createTextNode("Trace  of matrix: " + String(matA.trace())))
+    }
+}

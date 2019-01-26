6cda322283a19799420ce389a2f0311e083ef5b5
vetlewoie <vet.h.woie@hotmail.com>
2019-01-26 17:52:44 +0100

    ;

--- js/Matrix.js
+++ js/Matrix.js
@@ -90,7 +90,7 @@ class Matrix{
                 this.mulRow(op[1], op[2].inverse());
                 break;
             case 3:
-                this.addMulRow(op[1].inverse(),op[2],op[3])
+                this.addMulRow(op[1].inverse(),op[2],op[3]);
                 break;
         }
     }

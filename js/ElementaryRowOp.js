65b4ef6fc9b912890b7c3b5ee2983a259644005f
vetlewoie <vet.h.woie@hotmail.com>
2019-01-26 11:37:00 +0100

    Added general file with genreal methods which is used in every page

--- js/ElementaryRowOp.js
+++ js/ElementaryRowOp.js
@@ -1,37 +1,3 @@
-function newMatrix(inputMatrix, rows, columns){
-    var i,j;
-    var matrix = new Matrix(rows, columns);
-    for(i = 0; i < rows; i++){
-        for(j = 0; j < columns; j++){
-            matrix.mat[i][j] = new Rational(parseInt(inputMatrix[i][j].value),1);
-        }
-    }
-    return matrix;
-}
-
-function removeChildNodes(container){
-    while(container.hasChildNodes()){
-        container.removeChild(container.lastChild);
-    }
-}
-
-function parseString(string){
-    var fraction = 1;
-    var i;
-    for(i = 0; i < string.length -1; i++){
-        if(string[i] == '/'){
-            fraction = 0
-            break;
-        }
-    }
-    if(fraction == 1){
-        return new Rational(parseInt(string),1);
-    }else{
-        return new Rational(parseInt(string.substring(0,i)),parseInt(string.substring(i+1)));
-    }
-}
-
-
 function elementaryRow(){
     var rows = parseInt(document.getElementById('rows').value);
     var columns =parseInt(document.getElementById('columns').value);
